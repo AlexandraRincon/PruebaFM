5b7c54951b0cbdc61d3e540ba33f83c2b543c57d
AlexandraRincon <alexandra.03.ar@gmail.com>
2022-02-04 18:05:35 -0500

    Cabecera Resolucion pantalla  1920

--- src/Inicio/Inicio.js
+++ src/Inicio/Inicio.js
@@ -1,8 +1,68 @@
+import "./inicio.css";
+import monkey from "../Images/monkey.png";
+import fruit from "../Images/fruits.png";
+import download from "../Images/download.png";
+import play from "../Images/play.png";
 
-function Inicio(){
-    return(
-        <p>HOLA MUNDO</p>
-    );
+function Inicio() {
+  return (
+    <div>
+      <div className="background_Orange styleHeader">
+        <div className="head">
+          <div className="headMonkey">
+            <div className="headCircle"></div>
+            <img src={monkey} />
+          </div>
+          <div className="headMenu">
+            <div className="headerItems">
+              <a href="" className="">
+                Item 1
+              </a>
+              <a href="" className="">
+                Item 2
+              </a>
+              <a href="" className="">
+                Item 3
+              </a>
+            </div>
+            <div className="headerLogin">
+              <button className="headerLoginButtom"> Login</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="main">
+          <div className="mainDescription">
+            <div className="mainTitle">
+              <h1> Title Lorem ipsum. </h1>
+            </div>
+
+            <div className="mainSubtitle">
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas vehicula rutrum arcu in dictum. Praesent risus turpis.
+              </h3>
+            </div>
+          </div>
+
+          <div className="mainImagen">
+            <div className="mainCircle"></div>
+            <img src={fruit} />
+          </div>
+        </div>
+      </div>
+      <div className="actions">
+        <div className="actionDownload">
+          {" "}
+          <img src={download} />{" "}
+        </div>
+        <div className="actionsPlay">
+          {" "}
+          <img src={play} />{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Inicio;
