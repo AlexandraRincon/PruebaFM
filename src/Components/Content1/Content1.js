630894420008784737c08695ddcad001f459b684
AlexandraRincon <alexandra.03.ar@gmail.com>
2022-02-06 17:12:21 -0500

    contenido 2 con media queries

--- src/Components/Content1/Content1.js
+++ src/Components/Content1/Content1.js
@@ -1,40 +1,40 @@
 import "./content1.css";
 import Card from "../Card/Card";
+import mainOrange from "../../Images/mainorange.svg";
 
 function Content1() {
-
-  const scale={
-    width: "22%"
+  const scale = {
+    width: "22%",
   };
 
   return (
-    <div className="content">
-      <div className="contentTop widthHeight50">
+    <div className="content1">
+      <div className="contentTop widthHeight45">
         <div className="width50">
           <div className="contentTopLeft relativeCard">
-          <Card titlePosition="top" rotation={false}></Card>
-          </div>          
-        </div>
-        <div  className=" displayNone width50 ">
-          <div className="contentTopRigh relativeCard" style={scale}>
-          <Card titlePosition="top" rotation={true}></Card>
+            <Card titlePosition="top" rotation={false}></Card>
           </div>
-          
+        </div>
+        <div className=" displayNone width50 ">
+          <div className="contentTopRigh relativeCard" style={scale}>
+            <Card titlePosition="top" rotation={true}></Card>
+          </div>
         </div>
       </div>
-      <div className="contentBottom widthHeight50">
+      <div className="contentBottom widthHeight45">
         <div className=" displayNone width50">
           <div className="contentBottomLeft relativeCard" style={scale}>
-          <Card titlePosition="bottom" rotation={false}></Card>
+            <Card titlePosition="bottom" rotation={false}></Card>
           </div>
-          
         </div>
         <div className=" width50">
           <div className="contentBottomRight relativeCard">
-          <Card titlePosition="bottom" rotation={true}></Card>
+            <Card titlePosition="bottom" rotation={true}></Card>
           </div>
-          
         </div>
+      </div>
+      <div className="mainOrange">
+        <img src={mainOrange} alt="mainOrange" />
       </div>
     </div>
   );
--- src/Components/Content2/Content2.js
+++ src/Components/Content2/Content2.js
@@ -1,34 +1,65 @@
 import "./content2.css";
 import download2 from "../../Images/download.png";
 import celphone2 from "../../Images/celphoneMenu.svg";
-import bell from "../../Images/bell.svg"
+import bell from "../../Images/bell.svg";
+import salad from "../../Images/salad.svg";
+import calories from "../../Images/calories.svg";
+import food from "../../Images/food.svg";
+import recipes from "../../Images/recipes.svg";
+import mainOrange from "../../Images/mainorange.svg"
 function Content2() {
   return (
-    <div>
-      <div className="content2">
-        <div className="content2Img">
-          <div className="content2ImgCircle"></div>
-          <div className="content2ImgCelphone">
-            <img  src={celphone2} alt="celphone2" />
-            <div className="content2ImgBell"><img  src={bell} alt="bell" /></div>
-          </div>
-          
+    <div className="content2">
+
+      <div className="content2Below">
+        <div className="content2Download2">
+          <img src={download2} alt="download2" />
         </div>
-        <div className="content2Content">
-          <div className="content2Description">
-            <div className="content2Title"> Lorem ipsum.</div>
-            <div className="content2Parragraph">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Maecenas vehicula rutrum arcu in dictum. Praesent risus turpis.
-              </p>
-            </div>
-            <div className="content2Download">
-              <img src={download2} alt="download2" />
-            </div>
+        <div className="content2MainOrange">
+        <img src={mainOrange} alt="mainOrange" />
+      </div>
+      </div>
+
+
+
+      <div className="content2Img">
+
+        <div className="content2ImgCircle"></div>
+        <div className="content2ImgCelphone">
+          <img src={celphone2} alt="celphone2" />
+          <div className="content2ImgBell">
+            <img src={bell} alt="bell" />
+          </div>
+          <div className="content2ImgSalad">
+            <img src={salad} alt="salad" />
+          </div>
+          <div className="content2ImgRecipes">
+            <img src={recipes} alt="recipes" />
+          </div>
+          <div className="content2ImgFood">
+            <img src={food} alt="food" />
+          </div>
+          <div className="content2ImgCalories">
+            <img src={calories} alt="calories" />
+          </div>
+        </div>
+        
+      </div>
+      <div className="content2Content">
+        <div className="content2Description">
+          <div className="content2Title"> Lorem ipsum.</div>
+          <div className="content2Parragraph">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              vehicula rutrum arcu in dictum. Praesent risus turpis.
+            </p>
+          </div>
+          <div className="content2Download">
+            <img src={download2} alt="download2" />
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
